d12b01fe483cc20cefaeda68d7fbb13d3f1f303b
Kayw Lee <kwooler@gmail.com>
2024-02-13 20:25:43 -0800

    ASM

--- ASM/CH3P2Lab5.asm
+++ ASM/CH3P2Lab5.asm
@@ -1,7 +1,7 @@
 ## Program to represent 1.0
 
         .data
-val1:    .word  0x7F7FFFFF
+val1:    .word  0x7F7FFFFF  # Largest Number
 val2:    .float 1000.0
 	.text
         .globl  main
@@ -12,7 +12,8 @@ main:
     li		$t0, 0
     mtc1 	$t0, $f2        # $t0 = 0;
     cvt.s.w 	$f4, $f2    # Convert the double precision number to single precision
-    div.s 	$f9, $f4, $f4   # Largest Number / 0
+    l.s     $f6, val1
+    div.s 	$f9, $f6, $f4   # Largest Number / 0
     li 		$v0, 2		
     mfc1 	$t0, $f9
     mov.s 	$f12, $f9
