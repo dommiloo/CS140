835f7e9a58341b02ae2ccadbb508cd91ce2087d0
Kayw Lee <kwooler@gmail.com>
2024-02-13 20:33:50 -0800

    ASM

--- ASM/CH3P2Lab6.asm
+++ ASM/CH3P2Lab6.asm
@@ -1,7 +1,5 @@
-## Program: Largest Number / 0 = Infinity 
+## Program: 0 / 0 
 
-        .data
-val1:    .word  0x7F7FFFFF  # Largest Number
 	.text
         .globl  main
 
@@ -11,8 +9,7 @@ main:
     li		$t0, 0
     mtc1 	$t0, $f2        # $t0 = 0;
     cvt.s.w 	$f4, $f2    # Convert the double precision number to single precision
-    l.s     $f6, val1
-    div.s 	$f9, $f6, $f4   # Largest Number / 0
+    div.s 	$f9, $f4, $f4   # Largest Number / 0
     li 		$v0, 2		
     mfc1 	$t0, $f9
     mov.s 	$f12, $f9
