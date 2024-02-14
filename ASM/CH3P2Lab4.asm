2e16109bc9f73c8b7866c04c3c3ac89af867c023
Kayw Lee <kwooler@gmail.com>
2024-02-13 19:46:08 -0800

    ASM

---
+++ ASM/CH3P2Lab4.asm
@@ -0,0 +1,17 @@
+## Program to represent 1.0
+
+        .data
+val1:    .word  0x7FFFFFFF
+val2:    .float 1.0E3
+	.text
+        .globl  main
+
+main:
+    li $v0, 2 	# print floating service code
+    l.s	    $f5, val1
+    l.s 	$f7, val2 
+    mul.s 	$f9, $f5, $f7
+    li 	    $v0, 2		
+    mov.s   $f12, $f9
+    syscall
+## End of file
